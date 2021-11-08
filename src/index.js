f66c307738f009aff866d2a0f4ef3b21d66ecec5
csjv95 <csjv95@gmail.com>
2021-11-08 18:33:08 +0900

    Add reat-redux for more useful react redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import GlobalStyle from "./styles/GlobalStyle";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "./styles/Theme";
+import { createStore } from "redux";
+import rootReducer from "./redux/modules";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
+    <Provider store={store}>
     <ThemeProvider theme={Theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
